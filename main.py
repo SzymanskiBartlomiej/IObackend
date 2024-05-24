Fixed loading and reading file and normalization (minmax)

main.py
@@ -1,29 +1,18 @@
-# from fastapi import FastAPI
-# from csv_service import router
-
-# app = FastAPI()
-# app.include_router(router)
-
-
-
 import numpy as np
 from fastapi import APIRouter, HTTPException, FastAPI
 from fastapi.responses import JSONResponse
 import os
+import io
 import pandas as pd
-
-
-
-
-# data = None
-
-
-
 
 
 router = APIRouter()
 app = FastAPI()
 
+
+default_number_of_rows = 10
+
+#global variable for given data
 app.data = None
 
 
@@ -34,32 +23,37 @@ async def upload_file(filepath: str):
 
     if not filepath.endswith('.csv'):
         raise HTTPException(status_code=400, detail="File type not allowed")
-
-    # filename = os.path.basename(filepath)
-    # destination = os.path.join(UPLOAD_FOLDER, filename)
+    
+    data_string = ""
+    with open(filepath, 'r') as file:
+        data_string = file.read().replace(',', '.')
 
     #file to RAM
-    app.data = pd.read_csv(filepath)
-    app.data = app.data.iloc[1:]
+    app.data = pd.read_csv(io.StringIO(data_string), sep=";")
+
+    #replace , -> . in strings
+    # app.data.map(lambda string: string.replace(",", "."))
+
     app.data.replace({np.nan: None}, inplace=True)
 
-    # try:
-    #     shutil.copy(filepath, destination)
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=f"Failed to copy file: {str(e)}")
+    #conversion to numeric values
+    for column in app.data.columns:
+        try:
+            app.data[column] = pd.to_numeric(app.data[column])
+        except:
+            app.data[column] = pd.to_datetime(app.data[column])
 
     return {"message": "File successfully uploaded!"}
 
 
 
 @router.get("/readfile")
-async def read_file():
+async def read_file(n_of_rows: int):
     if app.data.empty:
         raise HTTPException(status_code=500, detail="No file found!")
     
     try:
-        # zwracamy tylko 20 pierwszych elementów
-        result = app.data.head(20).to_json()
+        result = app.data.head(n_of_rows).to_json(orient='records', lines=True)
         return JSONResponse(content=result)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -92,14 +86,15 @@ async def normalize_data(normalization: str):
     if normalization == "minmax":
         for column in normalized_data.columns:
             normalized_data[column] = (normalized_data[column] - normalized_data[column].min()) / (normalized_data[column].max() - normalized_data[column].min())      
-    if normalization == "other":
+    elif normalization == "other":
         pass
     else:
         raise HTTPException(status_code=400, detail=f"Normalization {normalization} not implemented!!")
 
     app.data = normalized_data
 
-    return read_file()
+    # return read_file(default_number_of_rows)
+    return {"message": f"Normalization completed successfully!"}
 
 
 @router.put("/pca")
@@ -107,10 +102,12 @@ async def pca_analysis():
     if app.data.empty:
         raise HTTPException(status_code=500, detail="No file found!")
     
-    return read_file()
+    return {"message": f"PCA completed successfully!"}
 
 
 
+
+#include router
 app.include_router(router)
 
 