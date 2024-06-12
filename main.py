b249fd0c4d481daec416aec96137c069f293c6f0
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-06-12 18:53:32 +0200

    Commit fix

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ async def rename_variables(old_name: str, new_name: str):
         try:
             app.numeric_data.rename(columns={old_name: new_name}, inplace=True)
             # app.normalized_data.rename(columns={old_name: new_name}, inplace=True)
-        except e:
+        except Exception as e:
             print("ojoj")
 
         return {"message": f"Variable '{old_name}' renamed to '{new_name}'"}
